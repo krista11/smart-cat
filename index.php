d156feabe2a2ae988625e7c66026dd1e12de5755
Kristina <kristina.staliauskaite@gmail.com>
2020-03-15 13:12:41 +0200

    index file changed

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-<?php echo " hello " ?>
+<?php
+
+$method == $_SERVER['REQUEST_METHOD'];
+
+if($method == "POST"){
+    $requestBody = file_get_contents('php://input');
+    $json = json_decode($requestBody);
+
+    $text = $json->queryResult->parameters->msg;
+
+    switch($text)
+    {
+        case 'hi':
+            $speech = "Hi, nice to meet you";;
+            break;
+        case 'bye':
+            $speech = "Bye, see you soon";
+            break;
+
+        default:
+            $speech = "Sorry, I didn't get that. Please ask me some else.";
+            break;
+    }
+
+    $response = new \stdClass();
+    $response->speech = "";
+    $response->displayText = "";
+    $response->source = "webhook";
+    echo json_encode($response);
+}
+else{
+    echo "Method not allowed";
+}
+
+?>
