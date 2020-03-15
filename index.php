e5fdfd06daa521e5a9ba32f90170db972e252e1f
Kristina <kristina.staliauskaite@gmail.com>
2020-03-15 15:54:03 +0200

    -

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
 <?php
+header('Content-Type: application/sjson'); 
+
+$requestBody = file_get_contents("php://input");
+    $json = json_decode($requestBody);
+
+    $text = $json->queryResult->parameters->msg;
+
+    switch($text)
+    {
+        case 'hi':
+            $speech = "Hi, nice to meet you";;
+            break;
+        case 'bye':
+            $speech = "Bye, see you soon";
+            break;
+
+        default:
+            $speech = "Sorry, I didn't get that. Please ask me some else.";
+            break;
+    }
+
+    $response = new \stdClass();
+    $response->speech = "";
+    $response->displayText = "";
+    $response->source = "webhook";
+    echo json_encode($response);
 
 
-print_r($_REQUEST);
 
 ?>
